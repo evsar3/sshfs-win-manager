8158b670faf7400b81e0b0185220fdee8cf78ce3
Evandro Araujo <evandroprogram@gmail.com>
2020-03-22 20:23:52 -0300

    Allow only one instance of the program at a time
    Fixed #2

--- src/main/index.js
+++ src/main/index.js
@@ -5,6 +5,9 @@ import windowManager from 'electron-window-manager'
 /* eslint-disable-next-line */
 import store from '../renderer/store'
 
+const isSecondInstance = !app.requestSingleInstanceLock()
+
+let mainWindow = null
 let tray = null
 
 if (process.env.NODE_ENV !== 'development') {
@@ -17,63 +20,74 @@ windowManager.init({
     : 'http://localhost:9080'
 })
 
-app.on('ready', () => {
-  const mainWindow = windowManager.createNew('main-window', '', '/index.html', null, {
-    title: 'SSHFS-Win Manager',
-    height: 500,
-    width: 900,
-    useContentSize: true,
-    frame: false,
-    maximizable: false,
-    minimizable: false,
-    resizable: false
-  }).create()
+if (isSecondInstance) {
+  app.quit()
+} else {
+  app.on('second-instance', () => {
+    if (mainWindow) {
+      mainWindow.object.show()
+      mainWindow.object.focus()
+    }
+  })
 
-  if (!process.argv.includes('--systray')) {
-    mainWindow.object.once('ready-to-show', () => {
+  app.on('ready', () => {
+    mainWindow = windowManager.createNew('main-window', '', '/index.html', null, {
+      title: 'SSHFS-Win Manager',
+      height: 500,
+      width: 900,
+      useContentSize: true,
+      frame: false,
+      maximizable: false,
+      minimizable: false,
+      resizable: false
+    }).create()
+
+    if (!process.argv.includes('--systray')) {
+      mainWindow.object.once('ready-to-show', () => {
+        mainWindow.object.show()
+      })
+    }
+
+    tray = new Tray(__static + '/tray-icon.ico')
+
+    const trayMenu = Menu.buildFromTemplate([
+      {
+        label: 'Quit',
+        click () {
+          mainWindow.object.webContents.send('terminate-child-processes')
+
+          ipcMain.on('child-processes-terminated', () => {
+            app.quit()
+          })
+        }
+      },
+      {
+        label: 'About',
+        click () {
+          const window = windowManager.createNew('about-window', '', '/index.html#about', null, {
+            height: 380,
+            width: 550,
+            useContentSize: true,
+            frame: false,
+            maximizable: false,
+            minimizable: false,
+            resizable: false,
+            modal: true,
+            parent: windowManager.get('main-window').object
+          }).create()
+
+          window.object.once('ready-to-show', () => {
+            window.object.show()
+          })
+        }
+      }
+    ])
+
+    tray.setToolTip('SSHFS-Win Manager')
+    tray.setContextMenu(trayMenu)
+
+    tray.on('click', () => {
       mainWindow.object.show()
     })
-  }
-
-  tray = new Tray(__static + '/tray-icon.ico')
-
-  const trayMenu = Menu.buildFromTemplate([
-    {
-      label: 'Quit',
-      click () {
-        mainWindow.object.webContents.send('terminate-child-processes')
-
-        ipcMain.on('child-processes-terminated', () => {
-          app.quit()
-        })
-      }
-    },
-    {
-      label: 'About',
-      click () {
-        const window = windowManager.createNew('about-window', '', '/index.html#about', null, {
-          height: 380,
-          width: 550,
-          useContentSize: true,
-          frame: false,
-          maximizable: false,
-          minimizable: false,
-          resizable: false,
-          modal: true,
-          parent: windowManager.get('main-window').object
-        }).create()
-
-        window.object.once('ready-to-show', () => {
-          window.object.show()
-        })
-      }
-    }
-  ])
-
-  tray.setToolTip('SSHFS-Win Manager')
-  tray.setContextMenu(trayMenu)
-
-  tray.on('click', () => {
-    mainWindow.object.show()
   })
-})
+}
