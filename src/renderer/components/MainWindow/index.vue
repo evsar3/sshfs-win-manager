841db591edc61e182ed145cbf9780c9bc76d384d
Evandro Araujo <evandroprogram@gmail.com>
2020-07-08 15:24:32 -0300

    Window resizing

--- src/renderer/components/MainWindow/index.vue
+++ src/renderer/components/MainWindow/index.vue
@@ -105,7 +105,7 @@ export default {
 
     addNewConnection () {
       const window = windowManager.createNew('add-new-connection-window', '', '/index.html#add-new-connection', null, {
-        height: 750,
+        height: 770,
         width: 500,
         useContentSize: true,
         frame: false,
@@ -123,7 +123,7 @@ export default {
 
     editConnection (conn) {
       const window = windowManager.createNew('edit-connection-window', '', `/index.html#edit-connection/${conn.uuid}`, null, {
-        height: 750,
+        height: 770,
         width: 500,
         useContentSize: true,
         frame: false,
