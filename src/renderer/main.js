a376a21fea95a697b08c756a2a9359b13989bb88
Evandro Araujo <evandroprogram@gmail.com>
2020-07-08 16:27:51 -0300

    V-tooltip component setup

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -4,9 +4,13 @@ import App from './App'
 import router from './router'
 import store from './store'
 
+import vToolTip from 'v-tooltip'
+
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 
 Vue.config.productionTip = false
+
+Vue.use(vToolTip)
 
 /* eslint-disable no-new */
 new Vue({
